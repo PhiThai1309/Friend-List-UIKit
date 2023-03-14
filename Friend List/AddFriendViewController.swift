e274016a93f16a6599704a72e1849d9b9d9d7afd
Phi Thai <thaimanhphi@gmail.com>
2023-03-14 17:37:43 +0700

    handle optional

--- Friend List/AddFriendViewController.swift
+++ Friend List/AddFriendViewController.swift
@@ -18,7 +18,8 @@ class AddFriendViewController: UIViewController {
         self.dismiss(animated: true)
     }
     @IBAction func AddOnClickHandle(_ sender: Any) {
-        if(EmailInputField.text != "" && NameInputField.text != "") {
+        if let email = EmailInputField.text, let name = NameInputField.text {
+            addFriend(name: name, email: email, gender: "Male")
             self.dismiss(animated: true)
         } else {
             let alert = UIAlertController(title: "Error!",
@@ -28,5 +29,33 @@ class AddFriendViewController: UIViewController {
             present(alert, animated: true, completion: nil)
         }
         
+    }
+    
+    func addFriend(name: String, email: String, gender: String) {
+        let url = URL(string: "https://gorest.co.in/public/v2/users")!
+        let token = "6e892f37b27d3e12257dabddab6806ffce8a0705d4dce4369ed94e672611b6ea"
+        
+        var request = URLRequest(url: url)
+        request.addValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
+        request.httpMethod = "POST"
+        
+        let task = URLSession.shared.dataTask(with: request) {
+            (data, response, error) in
+            if error == nil, let data = data, let response = response as? HTTPURLResponse {
+                
+                if (response.statusCode != 200) {
+                    return
+                }
+                
+                
+                DispatchQueue.main.async{
+//                    self.tableView.reloadData()
+                }
+                
+            } else {
+                print(error!)
+            }
+        }
+        task.resume()
     }
 }
