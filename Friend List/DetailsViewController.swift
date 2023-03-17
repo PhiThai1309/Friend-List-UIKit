19fcb0ca9e004a78a83c5fbd71686aea5119ac58
Phi Thai <thaimanhphi@gmail.com>
2023-03-17 13:46:37 +0700

    Add edit information in details view

--- Friend List/DetailsViewController.swift
+++ Friend List/DetailsViewController.swift
@@ -17,7 +17,6 @@ class DetailsViewController: UIViewController, UINavigationBarDelegate {
     @IBOutlet weak var genderField: UITextField!
     @IBOutlet weak var emailField: UITextField!
     @IBOutlet weak var nameField: UITextField!
-    @IBOutlet weak var idField: UITextField!
     @IBOutlet weak var imageView: UIImageView!
     
     var friend: Friend?
@@ -25,10 +24,9 @@ class DetailsViewController: UIViewController, UINavigationBarDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationBar.delegate = self
-        navigationTitle.title = friend?.name
+        navigationTitle.title = String(friend?.id ?? 0)
         
         if let value = friend {
-            idField.text = String(value.id!)
             nameField.text = value.name
             emailField.text = value.email
             genderField.text = value.gender
@@ -44,5 +42,64 @@ class DetailsViewController: UIViewController, UINavigationBarDelegate {
     
     func position(for bar: UIBarPositioning) -> UIBarPosition {
      return .topAttached
+    }
+    
+    func update(friend: Friend, id: Int) {
+        // Create a URLRequest for an API endpoint
+        let url = URL(string: "https://gorest.co.in/public/v2/users/" + String(id))!
+        let token = "6e892f37b27d3e12257dabddab6806ffce8a0705d4dce4369ed94e672611b6ea"
+        
+        // Convert model to JSON data
+        guard let jsonData = try? JSONEncoder().encode(friend) else {
+            print("Error: Trying to convert model to JSON data")
+            return
+        }
+        
+        var request = URLRequest(url: url)
+        request.addValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
+        request.httpMethod = "PATCH"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpBody = jsonData
+
+        let task = URLSession.shared.dataTask(with: request) {
+            (data, response, error) in
+            if error == nil, let data = data, let response = response as? HTTPURLResponse {
+                
+                if (response.statusCode != 200) {
+                    print("un-success")
+                    DispatchQueue.main.async {
+                        let alert = UIAlertController(title: "Error!",
+                                                      message: "Please check your input",
+                                                      preferredStyle: .alert)
+                        alert.addAction(UIAlertAction(title: "Return", style: .default))
+                        self.present(alert, animated: true, completion: nil)
+                    }
+                    return
+                } else {
+                    print("success")
+                    DispatchQueue.main.async {
+                        let alert = UIAlertController(title: "Success",
+                                                      message: "You have change the information successfully",
+                                                      preferredStyle: .alert)
+                        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: {_ in
+                            self.navigationController?.popViewController(animated: true)
+                        }))
+                        self.present(alert, animated: true, completion: nil)
+                    }
+                }
+                
+            } else {
+                print(error!)
+            }
+        }
+        task.resume()
+    }
+    
+    @IBAction func submitClickHandler(_ sender: Any) {
+        if let name = nameField.text, let email = emailField.text, let gender = genderField.text, let status = statusField.text {
+            var updateFriend = Friend(name: name, email: email, gender: gender, status: status)
+            update(friend: updateFriend, id: (friend?.id)!)
+        }
+        
     }
 }
